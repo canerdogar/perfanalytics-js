002653746c60a7865b27a45e6255b3e84f0f9a0a
canerdogar <canerdogar94@gmail.com>
2020-11-16 23:35:38 +0300

    gitignore added library name changed

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    library: '$',
+    filename: 'perfanalytics.js',
+    library: 'perfanalytics-js',
     libraryTarget: 'umd',
   },
   module: {
